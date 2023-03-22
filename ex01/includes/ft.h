7a8c4b3d6e85248ca38a2a837051e21292625737
Alê Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2023-03-22 19:56:09 +0000

    feat: Adds the folder 'ex01' and its files.

--- ex01/includes/ft.h
+++ ex01/includes/ft.h
@@ -1,18 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: arabelo- <arabelo-@Student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/22 11:35:09 by arabelo-          #+#    #+#             */
-/*   Updated: 2023/03/22 11:35:38 by arabelo-         ###   ########.fr       */
+/*   Created: 2023/03/22 16:47:03 by arabelo-          #+#    #+#             */
+/*   Updated: 2023/03/22 17:05:10 by arabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#ifndef FT_H
+# define FT_H
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
+int		ft_swap(int *a, int *b);
+
+#endif
